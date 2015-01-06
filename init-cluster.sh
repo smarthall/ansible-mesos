7f5447bc94b25efc46cbb5a589bd93f30da7fd3a
Daniel Hall <daniel@danielhall.me>
2015-01-06 23:59:24 +1100

    Adding example tasks

---
+++ init-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in task-data/marathon/*.json; do
+  echo "Deploying ${i} to Marathon"
+  curl -X POST -H "Content-Type: application/json" 'http://192.168.77.254:8080/v2/apps?force=true' -d@${i}
+  echo -e "\n"
+done
+
+for i in task-data/chronos/*.json; do
+  echo "Deploying ${i} to Chronos"
+  curl -L -H "Content-Type: application/json" -X POST -d@${i} http://192.168.77.254:4400/scheduler/iso8601
+  echo -e "\n"
+done
+
