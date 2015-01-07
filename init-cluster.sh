f81f7566bfca704e9b2cefb242f9b3214f486208
Daniel Hall <daniel@danielhall.me>
2015-01-07 22:45:12 +1100

    Silence like a ninja

--- init-cluster.sh
+++ init-cluster.sh
@@ -2,13 +2,11 @@
 
 for i in task-data/marathon/*.json; do
   echo "Deploying ${i} to Marathon"
-  curl -X POST -H "Content-Type: application/json" 'http://192.168.77.254:8080/v2/apps?force=true' -d@${i}
-  echo -e "\n"
+  curl -X POST -H "Content-Type: application/json" 'http://192.168.77.254:8080/v2/apps?force=true' -d@${i} &>/dev/null
 done
 
 for i in task-data/chronos/*.json; do
   echo "Deploying ${i} to Chronos"
-  curl -L -H "Content-Type: application/json" -X POST -d@${i} http://192.168.77.254:4400/scheduler/iso8601
-  echo -e "\n"
+  curl -L -H "Content-Type: application/json" -X POST -d@${i} http://192.168.77.254:4400/scheduler/iso8601 &>/dev/null
 done
 
